62bd4a877aee98f108cfcea56cfd4190fb9f08a5
Miruna-Miru <150169022+Miruna-Miru@users.noreply.github.com>
2024-02-16 22:45:56 +0530

    Create Exp1.sql

---
+++ Exp/Exp1.sql
@@ -0,0 +1,37 @@
+create table saleman22c075
+(
+  s_id int primary key,
+  s_nam varchar(7) not null,
+  city varchar(7) not null,
+  commission int not null
+  );
+
+create table customer22c075
+(
+  c_id int primary key,
+  c_nam varchar(7) not null,
+  grade int ,
+  city varchar(7) not null,
+  s_id int foreign key references saleman22c075(s_id)
+  );
+
+create table orders22c075
+(
+  o_no int  not null,
+  o_date date not null,
+  p_amt int not null,
+  s_id int foreign key references saleman22c075(s_id),
+  c_id int foreign key references customer22c075(c_id)
+  );
+
+insert into saleman22c075 values(1111,'Elena','Paris',10000);
+
+select * from saleman22c075;
+
+insert into saleman22c075 values(1112,'Amber','Rome',11000);
+
+insert into saleman22c075 values(1113,'Mira','Mdu',12000);
+
+insert into customer22c075 values(111,'Shin',9,'Paris',1111);
+
+select * from customer22c075;
